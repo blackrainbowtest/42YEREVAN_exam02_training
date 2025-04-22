795828c21b7027b87805dcfd0bcc478ceaf2bc11
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-04-22 23:23:27 +0400

    add

---
+++ level01/rev_print.c
@@ -0,0 +1,16 @@
+int main(int argc, char **argv)
+{
+	int i = 0;
+	if (argc == 2)
+	{
+		while (argv[1][i] != '\0')
+			i++;
+		while (i > 0)
+		{
+			i--;
+			
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
