466a5ff1765add3d88b7f10c85444037470df1d3
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-04-23 09:26:39 +0400

    add

--- level01/rotone.c
+++ level01/rotone.c
@@ -10,11 +10,11 @@ int main(int argc, char **argv)
 		{
 			if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
 			{
-				argv[1][i] = ((argv[1][i] - 'A' + 13) % 26) + 'A';
+				argv[1][i] = ((argv[1][i] - 'A' + 1) % 26) + 'A';
 			}
 			else if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
 			{
-				argv[1][i] = ((argv[1][i] - 'a' + 13) % 26) + 'a';
+				argv[1][i] = ((argv[1][i] - 'a' + 1) % 26) + 'a';
 			}
 			write(1, &argv[1][i], 1);
 			i++;
