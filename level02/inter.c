77ffac55dd8b9d167119f6e25dbe69ec94478916
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-04-26 10:24:18 +0400

    add

---
+++ level02/inter.c
@@ -0,0 +1,67 @@
+/*
+Assignment name  : inter
+Expected files   : inter.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes two strings and displays, without doubles, the
+characters that appear in both strings, in the order they appear in the first
+one.
+
+The display will be followed by a \n.
+
+If the number of arguments is not 2, the program displays \n.
+
+Examples:
+
+$>./inter "padinton" "paqefwtdjetyiytjneytjoeyjnejeyj" | cat -e
+padinto$
+$>./inter ddf6vewg64f gtwthgdwthdwfteewhrtag6h4ffdhsd | cat -e
+df6ewg4$
+$>./inter "rien" "cette phrase ne cache rien" | cat -e
+rien$
+$>./inter | cat -e
+$
+*/
+
+#include <unistd.h>
+
+int check_unic(char *str, char c, int pos)
+{
+	int i;
+
+	while (i < pos)
+	{
+		if (str[i] == c)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int main(int argc, char **argv)
+{
+
+	int i = 0;
+	int j = 0;
+	if (argc == 3)
+	{
+		while (argv[1][i] != '\0')
+		{
+			j = 0;
+			while (argv[2][j] != '\0')
+			{
+				if (argv[1][i] == argv[2][j])
+				{
+					if (check_unic(argv[1], argv[1][i], i))
+						write(1, &argv[1][i], 1);
+					break ;
+				}
+				j++;
+			}
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
